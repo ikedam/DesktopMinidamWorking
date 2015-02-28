918c0d0456d45f16b07ad147d3afb8669989796f
ikedam <devld@ikedam.jp>
2015-03-01 04:09:45 +0900

    修飾キー監視機能を追加

---
+++ DesktopMinidamWorking/BalloonWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopMinidamWorking
+{
+    /// <summary>
+    /// Interaction logic for BalloonWindow.xaml
+    /// </summary>
+    public partial class BalloonWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private string _message;
+        public string message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+                OnPropertyChanged("message");
+            }
+        }
+
+        public BalloonWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
--- DesktopMinidamWorking/MainWindow.xaml.cs
+++ DesktopMinidamWorking/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,19 +19,46 @@ namespace DesktopMinidamWorking
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private BalloonWindow balloonWindow;
+        private System.Windows.Threading.DispatcherTimer timer;
+        private ModifierKeys lastModifiers;
         // タスクトレイのアイコン
         // なぜか WPF ではサポートされないので、
         // ここだけ　WindowsForm。
         private System.Windows.Forms.NotifyIcon notifyIcon;
 
-        public bool menuTopmostChecked { get; set; }
+        private bool _menuTopmostChecked;
+        public bool menuTopmostChecked
+        {
+            get
+            {
+                return _menuTopmostChecked;
+            }
+            set
+            {
+                _menuTopmostChecked = value;
+                OnPropertyChanged("menuTopmostChecked");
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             // デザイナのプロパティをこのオブジェクトにバインド
             this.ContextMenu.DataContext = this;
+            this.menuTopmostChecked = true;
             // 画面右下に表示
             Top = System.Windows.SystemParameters.WorkArea.Top
                 + System.Windows.SystemParameters.WorkArea.Height
@@ -38,7 +66,16 @@ namespace DesktopMinidamWorking
             Left = System.Windows.SystemParameters.WorkArea.Left
                 + System.Windows.SystemParameters.WorkArea.Width
                 - Width;
+
+            balloonWindow = new BalloonWindow();
+            balloonWindow.Visibility = Visibility.Hidden;
+
             InitNotifyIcon();
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(33.3);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
 
         private void InitNotifyIcon()
@@ -73,6 +110,7 @@ namespace DesktopMinidamWorking
         private void MenuItemTopmost_CheckToggled(object sender, RoutedEventArgs e)
         {
             Topmost = menuTopmostChecked;
+            balloonWindow.Topmost = menuTopmostChecked;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -82,6 +120,51 @@ namespace DesktopMinidamWorking
                 notifyIcon.Dispose();
                 notifyIcon = null;
             }
+            if (balloonWindow != null)
+            {
+                balloonWindow.Close();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            ModifierKeys curModifier = Keyboard.Modifiers & (
+                ModifierKeys.Control
+                | ModifierKeys.Shift
+                | ModifierKeys.Alt
+            );
+            if (curModifier == lastModifiers)
+            {
+                return;
+            }
+            lastModifiers = curModifier;
+            List<string> messages = new List<string>();
+            if((Keyboard.Modifiers & ModifierKeys.Shift) != 0)
+            {
+                messages.Add("Shift");
+            }
+            if ((Keyboard.Modifiers & ModifierKeys.Control) != 0)
+            {
+                messages.Add("Control");
+            }
+            if ((Keyboard.Modifiers & ModifierKeys.Alt) != 0)
+            {
+                messages.Add("Alt");
+            }
+            if(messages.Count <= 0)
+            {
+                balloonWindow.Visibility = Visibility.Hidden;
+                balloonWindow.message = "";
+                return;
+            }
+            balloonWindow.message = string.Join("\n", messages);
+            balloonWindow.Show();
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            balloonWindow.Left = Left + Width / 2 - balloonWindow.Width / 2 - 20;
+            balloonWindow.Top = Top - balloonWindow.Height + 40;
         }
     }
 }
